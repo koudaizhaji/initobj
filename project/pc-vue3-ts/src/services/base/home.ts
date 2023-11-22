dd257c5cc4b7e3b86d327275d88ce74b7d354c7e
guoruifeng <guoruifeng@bjuci.com.cn>
2023-11-22 09:37:43 +0800

    feat(pc/menu): 添加创建编辑删除事件
    feat(pc/menu): 添加创建编辑删除事件

--- project/pc-vue3-ts/src/services/base/home.ts
+++ project/pc-vue3-ts/src/services/base/home.ts
@@ -4,3 +4,20 @@ export const getMenuList = () =>
   liliRequest.get({
     url: '/pcadmin/menu/list'
   })
+
+export const addMenu = <T>(data: T) =>
+  liliRequest.post({
+    url: '/pcadmin/menu/add',
+    data: data
+  })
+
+export const editMenu = <T>(data: T) =>
+  liliRequest.patch({
+    url: '/pcadmin/menu',
+    data: data
+  })
+
+export const deleteMenu = (id: string | number) =>
+  liliRequest.patch({
+    url: '/pcadmin/menu/' + id
+  })
