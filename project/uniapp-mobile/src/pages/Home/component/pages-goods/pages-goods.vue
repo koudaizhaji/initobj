aae2e3dbf577cd42ecbee71f6553e09c03fe2bc7
杨展鹏 <270663716@qq.com>
2024-01-08 17:44:55 +0800

    feat(uniapp):抽离Home组件和数据

---
+++ project/uniapp-mobile/src/pages/Home/component/pages-goods/pages-goods.vue
@@ -0,0 +1,62 @@
+<template>
+  <view>
+    <!-- 选项栏 -->
+    <view>
+      <view class="flex flex-justify-center flex-items-center mt-2 mb-2">
+        <view class="blackBox" />
+        <view class="ml-4 mr-4 font-size-4 font-bold">
+          严选推荐
+        </view>
+        <view class="blackBox" />
+      </view>
+      <view class="flex justify-center bg-white pl-4 pr-4">
+        <ul class="flex flex-justify-between w-100% h-12 p-0">
+          <li v-for="item in props.optionList" :key="item.id" @click="change(item.id)">
+            <view class="font-size-3.4 text-center mt-3 flex flex-items-center font-semibold">
+              {{ item.title }}
+              <view :class="item.icon" />
+            </view>
+          </li>
+        </ul>
+      </view>
+    </view>
+    <!-- 展示栏 -->
+    <view class="flex w-100%">
+      <view class="flex justify-center w-50% bg-white">
+        <img src="@/static/images/demo/cate_04.png" alt="">
+      </view>
+      <view class="flex justify-center w-50% bg-white">
+        <img src="@/static/images/demo/cate_06.png" alt="">
+      </view>
+    </view>
+  </view>
+</template>
+      
+<script setup>
+import { reactive} from 'vue'
+const props = defineProps({
+  optionList: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const change = (index) => {
+    index === 1 && props.optionList[0].icon === 'i-mdi-chevron-down' ? props.optionList[0].icon = 'i-mdi-chevron-up' : props.optionList[0].icon = 'i-mdi-chevron-down';
+    index === 2 && props.optionList[1].icon === 'i-mdi-chevron-down' ? props.optionList[1].icon = 'i-mdi-chevron-up' : props.optionList[1].icon = 'i-mdi-chevron-down';
+
+};
+</script>
+      
+<style lang="scss" scoped>
+.blackBox {
+  width: 12px;
+  height: 3px;
+  background-color: black;
+}
+
+li {
+  list-style: none;
+}
+</style>
+      
---
+++ project/uniapp-mobile/src/pages/Home/data/index.js
@@ -0,0 +1,53 @@
+// 头部导航信息
+export const navigationbarDataList = [
+  {
+    id: 1,
+    title: '拍拍严选',
+    qualityText: '专业质检',
+    warrantyText: '一年质保',
+    goodsReturn: '7天无理由',
+  },
+
+];
+// 分类页数据
+export const sortDataList = [
+  { id:1,icon: '/src/static/images/demo/cate_01.png', text: '手机数码' },
+  { id:2,icon: '/src/static/images/demo/cate_02.png', text: '买前必领' },
+  { id:3,icon: '/src/static/images/demo/cate_03.png', text: '一起拼团' },
+  { id:4,icon: '/src/static/images/demo/cate_04.png', text: '疯狂砍价' },
+  { id:5,icon: '/src/static/images/demo/cate_05.png', text: '限时秒杀' },
+  { id:6,icon: '/src/static/images/demo/cate_06.png', text: '分销中心' },
+  { id:7,icon: '/src/static/images/demo/cate_07.png', text: '我的收藏' },
+  { id:8,icon: '/src/static/images/demo/cate_08.png', text: '新闻资讯' },
+  { id:9,icon: '/src/static/images/demo/cate_09.png', text: '我的订单' },
+  { id:10,icon: '/src/static/images/demo/cate_01.png', text: '我的积分' },
+];
+
+// 严选推荐选项栏数据
+export const optionList = [
+  { id: 1, title: '综合排序', icon: 'i-mdi-chevron-down' },
+  { id: 2, title: '价格', icon: 'i-mdi-chevron-down' },
+  { id: 3, title: '机型', icon: 'i-mdi-cellphone-android' },
+  { id: 4, title: '筛选', icon: 'i-mdi-filter-settings-outline' },
+]
+// 卡片栏数据
+export const card_menu = [
+  {
+    img: '/src/static/swiper/swiper1.png',
+    title: '一加ACE',
+    author: '小明',
+    url: '/pages/detail/index?id=1',
+  },
+  {
+    img: '/src/static/swiper/swiper2.png',
+    title: '小米10',
+    author: '小红',
+    url: '/pages/detail/index?id=2',
+  },
+  {
+    img: '/src/static/swiper/swiper3.png',
+    title: '华为P40',
+    author: '小王',
+    url: '/pages/detail/index?id=3',
+  },
+]
