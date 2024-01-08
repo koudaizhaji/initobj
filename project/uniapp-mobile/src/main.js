1522a133804815541f497e4daa40cb3fffd37574
rabbit-lili <205500758@qq.com>
2024-01-08 16:36:20 +0800

    feat(uniapp-mobile): 新增手机设备验证识别

--- project/uniapp-mobile/src/main.js
+++ project/uniapp-mobile/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import 'virtual:uno.css';
 import tabBar from '@/components/tabBar.vue';
+import { isWeixinMobile } from '@/utils/public';
 
 const app = createSSRApp(App);
 
@@ -14,5 +15,20 @@ app.use(pinia);
 // Assuming MinRequest is a Vue plugin, you can use it like this:
 
 export function createApp() {
+  if (!isWeixinMobile()) {
+    // If not in WeChat, you can show a message or redirect here
+    uni.showToast({
+      title: '请使用手机微信打开本系统',
+      icon: 'none', // 'none'表示无图标
+      duration: 50000, // 显示时间2秒
+    });
+
+    // Optionally, you can exit the app
+    // uni.exit();
+    window.location.href = 'http://www.baidu.com';
+    // You can also redirect to another page
+    // window.location.href = 'https://yourRedirectUrl.com';
+    return null; // Return null to prevent the app from rendering
+  }
   return { app };
 }
--- project/uniapp-mobile/src/utils/public.js
+++ project/uniapp-mobile/src/utils/public.js
@@ -1,3 +1,29 @@
+// 环境验证
+// 手机设备判断
+const isMobile = () => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  if (ua.match(/android/i) == 'android' || ua.match(/iphone/i) == 'iphone' || ua.match(/ipad/i) == 'ipad') {
+    return true;
+  }
+  return false;
+};
+
+// 微信设备判断
+const isWeixin = () => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+    return true;
+  }
+  return false;
+};
+// 手机微信环境判断
+export const isWeixinMobile = () => {
+  if (isMobile() && isWeixin()) {
+    return true;
+  }
+  return false;
+};
+
 // 登录验证
 const goLogin = () => {
   // 回到上一页
