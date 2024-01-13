8d9095cf02fee05b9f0477d16a8c82d06b659e85
Elegy <270663716@qq.com>
2024-01-13 17:24:43 +0800

    feat:完善md编辑功能

--- project/pc-vue3-ts/src/views/template/form/markdown/markdown.vue
+++ project/pc-vue3-ts/src/views/template/form/markdown/markdown.vue
@@ -1,6 +1,14 @@
 <template>
+  <div class="sendMd">
+    <div class="inp">
+      <input type="text" placeholder="请输入标题" v-model="title">
+      <input type="text" placeholder="请输入描述" v-model="desc">
+    </div>
+    <button @click="logout">添加</button>
+    <button @click="getMd">查询</button>
+  </div>
   <div class="markdow-page">
-    <MdEditor v-model="text" />
+    <MdEditor v-model="content" />
   </div>
 </template>
 
@@ -12,11 +20,54 @@
 */
 import { ref } from 'vue';
 import { MdEditor } from 'md-editor-v3';
+import { addMd, findMdListById } from '@/services/markdown/markdown'
 import 'md-editor-v3/lib/style.css';
-let logout = () => {
-  console.log(text.value);
+
+let content = ref('');
+let title = ref('');
+let desc = ref('');
+
+let logout = async () => {
+  if (content.value.trim() === '') {
+    alert('文章的内容不能为空')
+    return
+  }
+  if (title.value.trim() === '') {
+    alert('文章的标题不能为空')
+    return
+  }
+  if (desc.value.trim() === '') {
+    alert('文章的描述不能为空')
+    return
+  }
+  let formData = {
+    title: title.value,
+    author: '挽歌',
+    desc: desc.value,
+    content: content.value,
+    classId: '99'
+  }
+  // console.log('formData', formData);
+  try {
+    let res = await addMd(formData)
+    content.value = ''
+    title.value = ''
+    desc.value = ''
+    console.log('存储结果', res);
+  } catch (error) {
+    console.log('err', error);
+  }
 }
-const text = ref('');
+
+let getMd = async () => {
+  try {
+    let res = await findMdListById('1', '10', '99')
+    console.log('请求结果', res);
+  } catch (error) {
+    console.log('请求错误', error);
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -24,8 +75,42 @@ const text = ref('');
   width: 100%;
   height: 100%;
 }
-ul, ol, li{
-  list-style:circle !important;
-  /* list-style:dot !important; */
+
+ul,
+ol,
+li {
+  list-style: circle !important;
+}
+
+.sendMd {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+
+  button {
+    width: 100px;
+    background-color: skyblue;
+    color: #fff;
+    border-radius: 24px;
+    border: none;
+  }
+
+  .inp {
+    width: 1000px;
+
+    input {
+      padding: 10px;
+    }
+
+    input:first-child {
+      width: 30%;
+      margin-right: 40px;
+    }
+
+    input:last-child {
+      width: 50%;
+    }
+  }
+
 }
 </style>
