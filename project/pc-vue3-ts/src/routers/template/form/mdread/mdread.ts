4bb5341da2c9293d4b8b13011df642c3f0d29d69
Elegy <270663716@qq.com>
2024-01-13 17:26:37 +0800

    feat:新增markdown展示页路由

---
+++ project/pc-vue3-ts/src/routers/template/form/mdread/mdread.ts
@@ -0,0 +1,7 @@
+const mdread = () => import('@/views/template/form/mdread/mdread.vue')
+export default {
+  path: '/template/form/mdread',
+  name: 'template-form-mdread',
+  component: mdread,
+  children: []
+}
--- project/pc-vue3-ts/src/views/template/form/mdread/mdread.vue
+++ project/pc-vue3-ts/src/views/template/form/mdread/mdread.vue
@@ -1,4 +1,4 @@
 <template>
-  <div>富文本编辑</div>
+  <div>md展示页面</div>
 </template>
 <script setup lang="ts"></script>
