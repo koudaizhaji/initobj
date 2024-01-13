b6915dd9d4767740534b8c58bd30ed0d518510a2
Elegy <270663716@qq.com>
2024-01-13 17:25:47 +0800

    feat:添加markdown模块接口

---
+++ project/pc-vue3-ts/src/services/markdown/markdown.ts
@@ -0,0 +1,41 @@
+/*
+ * @Author: 挽歌
+ * @Date: 2024-01-13
+ * @Description: markdown模块前端接口
+ */
+import liliRequest from '../index'
+// 添加markdown
+export function addMd(formdata: object) {
+  return liliRequest.post({
+    url: '/pcadmin/set/markdown/add',
+    data: formdata
+  })
+}
+// 查询md内容列表
+export function findMdListById(pageNum:string,pageSize:string,classId:string){
+  return liliRequest.post({
+    url: '/pcadmin/set/markdown/list',
+    data: {
+      pageNum,
+      pageSize,
+      classId
+    }
+  })
+}
+// 添加md的分类
+export function addMdClass(formdata: object){
+  return liliRequest.post({
+    url: '/pcadmin/set/markdownClass/add',
+    data: formdata
+  })
+}
+// 查询md的分类
+export function findMdClass(pageNum:string,pageSize:string){
+  return liliRequest.post({
+    url: '/pcadmin/set/markdownClass/list',
+    data: {
+      pageNum,
+      pageSize
+    }
+  })
+}
