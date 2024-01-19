c7898f84f77d6bb2822ce8d67e5bd409db7554ed
Elegy <270663716@qq.com>
2024-01-19 18:06:03 +0800

    feat(pc-vue3-ts): markdown编辑页面弹窗封装

--- project/pc-vue3-ts/src/views/template/form/markdown/component/PopUp.vue
+++ project/pc-vue3-ts/src/views/template/form/markdown/component/PopUp.vue
@@ -3,61 +3,127 @@
   <el-button text @click="dialogFormVisible = true">
     <slot></slot>
   </el-button>
-
-  <el-dialog v-model="dialogFormVisible" title="提交文章">
+  <el-dialog v-model="dialogFormVisible" :title="options.action">
     <el-form :model="form">
-      <el-form-item label="文章标题" :label-width="formLabelWidth">
-        <el-input v-model="form.title" autocomplete="off" />
+      <el-form-item :label="options.title" :label-width="formLabelWidth">
+        <el-input v-model="form.title" autocomplete="off" placeholder="请输入标题/名称" />
       </el-form-item>
-      <el-form-item label="文章简介" :label-width="formLabelWidth">
-        <el-input v-model="form.desc" autocomplete="off" />
+      <el-form-item :label="options.desc" :label-width="formLabelWidth">
+        <el-input v-model="form.desc" autocomplete="off" placeholder="请输入描述"/>
       </el-form-item>
-      <el-form-item label="文章分类" :label-width="formLabelWidth">
+      <el-form-item v-if="Atype==='1'" label="文章分类" :label-width="formLabelWidth">
         <el-select v-model="form.type" placeholder="请选择文章分类">
-          <el-option label="私人空间" value="分类1" />
-          <el-option label="公共空间" value="分类2" />
+          <el-option v-for="item in mdType" :key="item.id" :label="item.name" :value="item.id" />
         </el-select>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">
-          提交
-        </el-button>
+        <el-button type="primary" @click="add(Atype)">提交</el-button>
       </span>
     </template>
   </el-dialog>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
-
-const dialogTableVisible = ref(false)
+import { reactive, ref , defineProps,onMounted } from 'vue'
+import {findMdClass,addMd, addMdClass} from '@/services/markdown/markdown'
+let mdType=[]
+onMounted(async()=>{
+  let res=await findMdClass('1','10')
+  for(let i=0;i<res.data.length;i++){
+      mdType.push({
+        id:res.data[i].id,
+        name:res.data[i].name
+      })
+  }
+})
+const props=defineProps({
+  options:{
+    type:Object
+  },
+  content:{
+    type:String,
+    default:''
+  },
+  Atype:{
+    type:String
+  }
+})
 const dialogFormVisible = ref(false)
 const formLabelWidth = '140px'
 
 const form = reactive({
   title: '',
   desc: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  resource: '',
+  type:'',
 })
+
+let add = async (type:string) => {
+  let flag=type==='1'?'文章':'分类'
+  if (form.title.trim() === '') {
+    alert(`${flag}的标题不能为空`)
+    return
+  }
+  if (form.desc.trim() === '') {
+    alert(`${flag}的描述不能为空`)
+    return
+  }
+  if(type==='1'){
+    if (props.content.trim() === '') {
+    alert('文章的内容不能为空')
+    return
+  }
+  let formData = {
+    title: form.title,
+    author: '挽歌',
+    desc: form.desc,
+    content: props.content,
+    classId: form.type
+  }
+  try {
+    let res = await addMd(formData)
+    form.title = ''
+    form.desc = ''
+    console.log('文章存储结果', res);
+  } catch (error) {
+    console.log('err', error);
+  }
+  }else{
+    let formData = {
+    name: form.title,
+    desc: form.desc,
+    status:'1',
+    type:'1',
+    userId: '2'
+  }
+  // console.log('formData', formData);
+  try {
+    let res = await addMdClass(formData)
+    form.title = ''
+    form.desc = ''
+    console.log('分类存储结果', res);
+  } catch (error) {
+    console.log('err', error);
+  }
+  }
+}
 </script>
 <style scoped>
-.el-button,.el-button:active,.el-button:hover,.el-button.is-text:not(.is-disabled):focus, .el-button.is-text:not(.is-disabled):hover{
+.el-button,
+.el-button:active,
+.el-button:hover,
+.el-button.is-text:not(.is-disabled):focus,
+.el-button.is-text:not(.is-disabled):hover {
   padding: 20px 30px;
-    background-color:#1D7DFA;
-    color: #fff;
-    border-radius: 2px;
-    border: none;
-    font-size: 16px;
-    margin: 0 10px;
+  margin: 0 10px;
+  font-size: 16px;
+  color: #fff;
+  background-color: #1d7dfa;
+  border: none;
+  border-radius: 2px;
 }
-
 
 /* .el-button--text {
   margin-right: 15px;
@@ -65,13 +131,17 @@ const form = reactive({
 .el-select {
   width: 300px;
 }
+
 .el-input {
   width: 300px;
 }
+
 .dialog-footer button:first-child {
   margin-right: 10px;
 }
-.el-dialog__footer,.el-dialog{
+
+.el-dialog__footer,
+.el-dialog {
   text-align: center !important;
 }
 </style>
--- project/pc-vue3-ts/src/views/template/form/markdown/markdown.vue
+++ project/pc-vue3-ts/src/views/template/form/markdown/markdown.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="sendMd">
-    <!-- <div class="inp">
-      <input type="text" placeholder="请输入文章标题" v-model="title">
-      <input type="text" placeholder="请输入文章描述" v-model="desc">
-    </div> -->
-    <PopUp @click="addMsg">添加文章</PopUp>
-    <PopUp @click="addClass">添加分类</PopUp>
-    <!-- <PopUp @click="getMd">查询</PopUp> -->
-    <!-- <button >添加文章</button> -->
-    <!-- <button @click="getMd">查询</button> -->
+    <PopUp :content="content" :options="markdown" Atype="1">添加文章</PopUp>
+    <PopUp :content="content" :options="classtype" Atype="2">添加分类</PopUp>
   </div>
-  <!-- <div class="sendMd">
-    <div class="inp">
-      <input type="text" placeholder="请输入分类标题" v-model="classTitle">
-      <input type="text" placeholder="请输入分类描述" v-model="classDesc">
-    </div>
-    <PopUp @click="addClass">添加分类</PopUp>
-    <button @click="addClass">添加分类</button>
-  </div> -->
   <div class="markdow-page">
     <MdEditor v-model="content" />
   </div>
@@ -32,74 +17,10 @@
 import { ref } from 'vue';
 import { MdEditor } from 'md-editor-v3';
 import PopUp from './component/PopUp.vue'
-// import pageModal from '@/components/pages/page-modal/page-modal.vue'
-// import modalConfig from './config/modal.config.ts'
-// import usePageModal from '@/hooks/usePageModal'
 import { addMd, addMdClass } from '@/services/markdown/markdown'
+import {markdown,classtype} from './config/model.config.js'
 import 'md-editor-v3/lib/style.css';
 let content = ref('');
-let title = ref('');
-let desc = ref('');
-let classTitle = ref('');
-let classDesc = ref('');
-let addMsg = async () => {
-  if (content.value.trim() === '') {
-    alert('文章的内容不能为空')
-    return
-  }
-  if (title.value.trim() === '') {
-    alert('文章的标题不能为空')
-    return
-  }
-  if (desc.value.trim() === '') {
-    alert('文章的描述不能为空')
-    return
-  }
-  let formData = {
-    title: title.value,
-    author: '挽歌',
-    desc: desc.value,
-    content: content.value,
-    classId: '2'
-  }
-  // console.log('formData', formData);
-  try {
-    let res = await addMd(formData)
-    content.value = ''
-    title.value = ''
-    desc.value = ''
-    console.log('文章存储结果', res);
-  } catch (error) {
-    console.log('err', error);
-  }
-}
-let addClass =async ()=>{
-  if (classTitle.value.trim() === '') {
-    alert('分类的标题不能为空')
-    return
-  }
-  if (classDesc.value.trim() === '') {
-    alert('分类的描述不能为空')
-    return
-  }
-  let formData = {
-    name: classTitle.value,
-    desc: classDesc.value,
-    status:'1',
-    type:'1',
-    userId: '2'
-  }
-  // console.log('formData', formData);
-  try {
-    let res = await addMdClass(formData)
-    classTitle.value = ''
-    classDesc.value = ''
-    console.log('分类存储结果', res);
-  } catch (error) {
-    console.log('err', error);
-  }
-}
-
 </script>
 
 <style lang="scss" scoped>
